74fe1384cb93539f60e481fba27cee775e083950
Unknown <dunia.abunaaj@gmail.com>
2017-09-25 14:29:04 +0200

    getprogile+test

--- src/mm/androidservice/GetProfile.java
+++ src/mm/androidservice/GetProfile.java
@@ -1,45 +1,129 @@
 package mm.androidservice;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.MediaType;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.sql.SQLException;
+import java.time.Instant;
+import java.util.Iterator;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import controllers.SessionController;
 import mm.constants.Constants;
 import mm.da.DataAccess;
 import mm.model.JsonUser;
 import mm.model.User;
 
-@Path("/users")
-public class GetProfile {
+/**
+ * Servlet implementation class LogInTest
+ */
+@WebServlet("/getProfile")
+public class GetProfile extends HttpServlet {
+	private static final long serialVersionUID = 1L;
 	
-	
-
-	@GET
-	@Path("/getProfile")
-	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_JSON)
-	public JsonUser getProfile(@QueryParam("id") String id,@QueryParam("token") String token) {
-		JsonUser jsonUser;
-		User user;
-
+	private class UserSession {
 		
-		DataAccess da = new DataAccess();
-		//user=da.getUser(id);//returns a user or null or session not available
-		user=null;
-		if(user==null) {
-			jsonUser=new JsonUser(null,Constants.STATUS_MISSINGPARA,Constants.USERNOTFOUND,token);
+		private String id;
+		private String token;
+	
+		public UserSession(String email, String session) {
+			super();
+			this.id = email;
+			this.token=session;
 		}
-		else {
-			jsonUser=new JsonUser(user,Constants.STATUS_SUCCESS,Constants.SUCCESS,token);
-
+		@Override
+		public String toString() {
+			return "UserSession [email=" + id + ", session=" + token +"]";
 		}
 		
-		return jsonUser;
 		
+		
+
 	}
 	
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public GetProfile() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	//	response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		doGet(request, response);
+		
+		    
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		BufferedReader br = new BufferedReader(
+		        new InputStreamReader(request.getInputStream(), StandardCharsets.UTF_8));
+
+		    StringBuilder sb = new StringBuilder();
+		    String s;
+		    while ((s = br.readLine()) != null) {
+		         sb.append(s).append("\n");
+		    }
+
+		    String jsonString = sb.toString();
+		    Gson gson = new Gson();
+		    UserSession myUser = gson.fromJson( jsonString, UserSession.class ); 
+			
+			
+		    response.getWriter().append(jsonString);
+		    
+
+			
+		//	PrintWriter writer = 
+
+			
+			DataAccess da = new DataAccess();
+			User user = null;
+			JsonUser jsonUser=null;
+
+			if (user == null) {
+				
+
+
+				jsonUser = new JsonUser(user, Constants.STATUS_MISSINGPARA, Constants.USERNOTFOUND, null);
+			} else {
+				
+				jsonUser=new JsonUser(user,Constants.STATUS_SUCCESS,Constants.SUCCESS,myUser.token);
+				
+				}
+
+			
+			response.setContentType("application/json");
+			// Get the printwriter object from response to write the required json object to the output stream      
+			PrintWriter out = response.getWriter();
+			// Assuming your json object is **jsonObject**, perform the following, it will return your json object  
+			out.print(jsonUser);
+			out.flush();
+			out.close();
+		
+	}
+
 }
