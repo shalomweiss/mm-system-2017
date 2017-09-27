d9f6c1d57fb1caae1578961c3e395ca4afe87682
Unknown <firdos.cs94@gmail.com>
2017-09-27 13:08:25 +0200

    delete jsonuser from model+ depre

---
+++ src/mm/androidservice/UpdateProfile.java
@@ -0,0 +1,96 @@
+package mm.androidservice;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+import mm.constants.Constants;
+import mm.da.DataAccess;
+import mm.jsonModel.JsonUser;
+import mm.model.User;
+import util.ServerUtils;
+
+
+/**
+ * Servlet implementation class UpdateProfileTest
+ */
+@WebServlet("/UpdateProfile")
+public class UpdateProfile extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	/*for json parsing*/
+	private class UserSession {
+		
+		private String id;
+		private String token;
+		private User updatedUser;
+		public UserSession(String id, String token, User updatedUser) {
+			super();
+			this.id = id;
+			this.token = token;
+			this.updatedUser = updatedUser;
+		}
+		@Override
+		public String toString() {
+			return "UserSession [id=" + id + ", token=" + token + ", updatedUser=" + updatedUser + "]";
+		}
+		
+		
+
+	}
+	
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UpdateProfile() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+//	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		// TODO Auto-generated method stub
+//		doGet(request, response);
+//	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+		    UserSession userToUpdate = ServerUtils.getJsonFromRequest(request,UserSession.class);
+		    User user = null;
+			DataAccess da = new DataAccess();
+			//TODO
+			//user=da.updateProfile(userToUpdate.id,userToUpdate.token,userToUpdate.updatedUser);
+			JsonUser jsonUser=null;
+			
+			if(user==null) {
+				jsonUser=new JsonUser(user, Constants.STATUS_MISSINGPARA, Constants.USERNOTFOUND, userToUpdate.token);
+			}
+			else {
+				jsonUser=new JsonUser(user, Constants.STATUS_SUCCESS, Constants.SUCCESS, userToUpdate.token);
+			}
+			
+			ServerUtils.respondJsonUser(response,jsonUser);
+	}
+	
+
+	
+	
+
+}
