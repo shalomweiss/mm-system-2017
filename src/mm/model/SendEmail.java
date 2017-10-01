f3b75d8d75781bdc20d5ab93cfe15dd69a7a253c
Unknown <ghada.8.a@gmail.com>
2017-10-01 13:20:13 +0300

    FIX

---
+++ src/mm/model/SendEmail.java
@@ -0,0 +1,10 @@
+package mm.model;
+import java.util.*;
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.activation.*;
+
+
+public class SendEmail {
+
+}
---
+++ src/mm/webclientservlets/SendPairUpdates.java
@@ -0,0 +1,41 @@
+package mm.webclientservlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import mm.da.DataAccess;
+import mm.model.Pair;
+
+/**
+ * Servlet implementation class SentPairUpdates
+ */
+@WebServlet("/SentPairUpdates")
+public class SendPairUpdates extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SendPairUpdates() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		System.out.println("GetPairDetails Servlet");
+		String nextPage = request.getParameter("jsp");
+		int pairId = Integer.parseInt(request.getParameter("pairId"));
+		DataAccess da = new DataAccess();
+		String updates  = da.getUpdates(pairId);
+		request.setAttribute("Updates", updates);
+	}
+	
+
+}
