97742b1fb444a2731495169538249e322c3a6662
kanaani <m.y.k.224@gmail.com>
2017-10-09 10:53:54 +0300

    testing and cleaning

--- src/mm/tests/db/getAllAcademiclnstitution.java
+++ src/mm/tests/db/getAllAcademiclnstitution.java
@@ -1,9 +1,11 @@
 package mm.tests.db;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 import mm.da.DataAccess;
 import mm.model.AcademicInstitute;
+import mm.model.Mentee;
 
 public class getAllAcademiclnstitution {
 
@@ -12,8 +14,10 @@ public class getAllAcademiclnstitution {
 	public static void main(String[] args) throws SQLException {
 		// TODO Auto-generated method stub
 		da = new DataAccess();
-		for(AcademicInstitute s:da.getAllAcademiclnstitution())
-				System.out.println( s.toString());
+		//for(AcademicInstitute s:da.getAllAcademiclnstitution())
+			//System.out.println( s.toString());
+		ArrayList<Mentee> mentees = da.getMenteesOfMentor(17);
+			System.out.println(mentees);
 	}
 
 }
