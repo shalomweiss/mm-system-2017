3af9b6c88292b7d4126fe0609bbf71658fb75dcb
Unknown <foad.ad5491@gmail.com>
2017-11-20 08:40:07 +0200

    mistakeeeee

--- src/mm/androidservice/GetMeetings.java
+++ src/mm/androidservice/GetMeetings.java
@@ -30,7 +30,6 @@ public class GetMeetings extends HttpServlet {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
 	 *      response)
